9b0bdb17e51dfd4f3a26058d560bb4d81c67699a
Corwin Kuiper <corwin@kuiper.dev>
2021-03-06 20:09:23 +0000

    implement more ways of panicking

--- examples/panic.rs
+++ examples/panic.rs
@@ -14,8 +14,13 @@ fn main(_argc: isize, _argv: *const *const u8) -> isize {
 
     loop {
         input.update();
+        // if A is pressed, draw out of range
         if input.is_just_pressed(gba::input::Button::A) {
             bitmap.draw_point(display::WIDTH, 0, 0x05);
+        }
+        // if B is pressed, try take another bitmap
+        if input.is_just_pressed(gba::input::Button::B) {
+            gba.display.bitmap4();
         }
     }
 }
