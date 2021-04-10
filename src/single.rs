57db4edeb83d365c4953ba5b03193f6cfff00f81
Corwin Kuiper <corwin@kuiper.dev>
2021-04-10 22:22:56 +0100

    add what's required for cargo test

--- src/single.rs
+++ src/single.rs
@@ -6,9 +6,16 @@ impl<T> Singleton<T> {
     pub const fn new(s: T) -> Self {
         Singleton { single: Some(s) }
     }
+    pub const fn empty() -> Self {
+        Singleton { single: None }
+    }
     pub fn take(&mut self) -> T {
         let g = core::mem::replace(&mut self.single, None);
         g.unwrap()
+    }
+
+    pub fn ret(&mut self, ret: T) {
+        self.single = Some(ret);
     }
 }
 
