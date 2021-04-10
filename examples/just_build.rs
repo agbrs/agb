adfa3b9fded8e719fff211c51d266101ec0a3cb2
Corwin Kuiper <corwin@kuiper.dev>
2021-04-10 20:02:51 +0100

    add example that is literally the tonc example

--- examples/just_build.rs
+++ examples/just_build.rs
@@ -2,7 +2,7 @@
 #![no_main]
 
 #[panic_handler]
-fn panic_handler(info: &core::panic::PanicInfo) -> ! {
+fn panic_handler(_info: &core::panic::PanicInfo) -> ! {
     loop {}
 }
 
@@ -15,8 +15,8 @@ pub fn main() -> ! {
         *(0x0400_0000 as *mut u32) = 0x0403;
         let video = 0x0600_0000 as *mut u16;
         *video.offset(120 + 80 * 240) = 0x001F;
-        *video.offset(136 + 80 * 240) = 0x001F;
-        *video.offset(120 + 96 * 240) = 0x001F;
+        *video.offset(136 + 80 * 240) = 0x03E0;
+        *video.offset(120 + 96 * 240) = 0x7C00;
     }
     loop {}
 }
