4b119e794d1ec2f55d1524511e1d910f2716ae6e
Corwin Kuiper <corwin@kuiper.dev>
2021-04-11 16:35:49 +0100

    remove unused code and allow dead code

--- src/single.rs
+++ src/single.rs
@@ -2,6 +2,7 @@ pub struct Singleton<T> {
     single: Option<T>,
 }
 
+#[allow(dead_code)]
 impl<T> Singleton<T> {
     pub const fn new(s: T) -> Self {
         Singleton { single: Some(s) }
@@ -12,40 +13,5 @@ impl<T> Singleton<T> {
     pub fn take(&mut self) -> T {
         let g = core::mem::replace(&mut self.single, None);
         g.unwrap()
-    }
-
-    pub fn ret(&mut self, ret: T) {
-        self.single = Some(ret);
-    }
-}
-
-pub struct Single {
-    is_taken: bool,
-}
-
-pub struct SingleToken<'a> {
-    cell: &'a mut bool,
-}
-
-impl Single {
-    pub const fn new() -> Self {
-        Single { is_taken: false }
-    }
-
-    pub fn take(&mut self) -> Result<SingleToken, &'static str> {
-        if self.is_taken {
-            Err("Already taken")
-        } else {
-            self.is_taken = true;
-            Ok(SingleToken {
-                cell: &mut self.is_taken,
-            })
-        }
-    }
-}
-
-impl Drop for SingleToken<'_> {
-    fn drop(&mut self) {
-        (*self.cell) = false;
     }
 }
