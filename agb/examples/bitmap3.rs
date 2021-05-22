c14dd2cd33b5f5bb07f0d9f8c2cf45a5211384e9
Corwin Kuiper <corwin@kuiper.dev>
2021-05-22 21:22:44 +0100

    change entrypoint signature of all examples

--- agb/examples/bitmap3.rs
+++ agb/examples/bitmap3.rs
@@ -1,5 +1,5 @@
 #![no_std]
-#![feature(start)]
+#![no_main]
 
 extern crate agb;
 
@@ -10,8 +10,8 @@ struct Vector2D {
     y: i32,
 }
 
-#[start]
-fn main(_argc: isize, _argv: *const *const u8) -> isize {
+#[no_mangle]
+pub fn main() -> ! {
     let mut gba = agb::Gba::new();
     let mut bitmap = gba.display.video.bitmap3();
     let vblank = gba.display.vblank.get();
