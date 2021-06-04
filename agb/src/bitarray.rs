9fad597844c3b7a9da3aab9f3a0f18c2eebad056
Corwin Kuiper <corwin@kuiper.dev>
2021-06-04 18:36:08 +0100

    fix faulty implementation

--- agb/src/bitarray.rs
+++ agb/src/bitarray.rs
@@ -9,7 +9,7 @@ impl<const N: usize> Bitarray<N> {
 
     pub fn get(&self, index: usize) -> Option<bool> {
         if index < N * 32 {
-            Some((self.a[index / N] >> (N % 32) & 1) != 0)
+            Some((self.a[index / 32] >> (N % 32) & 1) != 0)
         } else {
             None
         }
@@ -19,7 +19,7 @@ impl<const N: usize> Bitarray<N> {
         let value = value as u32;
         let mask = 1 << (N % 32);
         let value_mask = value << (N % 32);
-        self.a[index / N] = self.a[index / N] & !mask | value_mask
+        self.a[index / 32] = self.a[index / 32] & !mask | value_mask
     }
 }
 
