466d0844c5023acf8a8917dafa2650d649dfb4e1
Corwin Kuiper <corwin@kuiper.dev>
2021-06-23 23:09:36 +0100

    push all registers, as rust may be using them

--- agb/interrupt_simple.s
+++ agb/interrupt_simple.s
@@ -22,11 +22,11 @@ InterruptHandlerSimple:
 
     @ call the rust interrupt handler with r0 set to the triggered interrupts
     ldr r1, =__RUST_INTERRUPT_HANDLER
-    push {lr, r4}
+    push {lr, r4-r11}
     adr lr, .IReturn
     bx r1
 .IReturn:
-    pop {lr, r4}
+    pop {lr, r4-r11}
 
     @ change back to interuupt mode
     mrs r2, cpsr
