469db774371d75cdb031e7d9c892cef0c4a3a068
Gwilym Kuiper <gw@ilym.me>
2021-08-16 23:28:33 +0100

    Add allocation example

---
+++ agb/examples/allocation.rs
@@ -0,0 +1,15 @@
+#![no_std]
+#![no_main]
+
+extern crate agb;
+extern crate alloc;
+
+use alloc::boxed::Box;
+
+#[agb::entry]
+fn main() -> ! {
+    loop {
+        let b = Box::new(1);
+        agb::println!("dynamic allocation made to {:?}", &*b as *const _);
+    }
+}
