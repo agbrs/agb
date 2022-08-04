bf3021c6d9a1de672ae82810d49917574c716b7a
Gwilym Kuiper <gw@ilym.me>
2022-08-04 20:28:14 +0100

    Basic publish command that just prints 'publishing'

---
+++ tools/src/main.rs
@@ -0,0 +1,22 @@
+use clap::{command, Parser};
+
+#[derive(Parser, Debug)]
+struct PublishAllCratesArgs {}
+
+fn main() {
+    let matches = command!()
+        .propagate_version(true)
+        .subcommand_required(true)
+        .arg_required_else_help(true)
+        .subcommand(clap::Command::new("publish").about("Publishes agb and all subcrates"))
+        .get_matches();
+
+    match matches.subcommand() {
+        Some(("publish", _)) => publish(),
+        _ => unreachable!("Exhausted list of subcommands and subcommand_required prevents `None`"),
+    }
+}
+
+fn publish() {
+    println!("publishing!");
+}
