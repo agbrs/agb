4dfa6c25a588a20d05d772bcfb616d037a7c445f
Corwin <corwin@kuiper.dev>
2022-10-09 17:27:07 +0100

    REMOVE UNUSED IMPORT THAT I JUST MADE

--- agb/src/syscall.rs
+++ agb/src/syscall.rs
@@ -2,7 +2,7 @@ use agb_fixnum::Vector2D;
 use core::arch::asm;
 use core::mem::MaybeUninit;
 
-use crate::display::affine::{AffineMatrixBackground, AffineMatrixObject};
+use crate::display::affine::AffineMatrixBackground;
 use crate::fixnum::Num;
 
 #[allow(non_snake_case)]
