4a630fe85b590f45c993b8d2650420b19b7999af
Corwin <corwin@kuiper.dev>
2023-04-13 21:44:00 +0100

    prettier'd

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -1,18 +1,28 @@
-import React, { useState } from 'react';
-import { Mgba } from './mgba';
+import React, { useState } from "react";
+import { Mgba } from "./mgba";
 
 function App() {
-
   const [onGame, setOnGame] = useState(false);
   const [volume, setVolume] = useState(1.0);
 
   return (
     <div>
-      {
-        onGame && <><Mgba gameUrl="/game.gba" volume={volume} />
-          <input type="range" value={volume} min="0" max="1" step="0.05" onChange={(e) => setVolume(Number(e.target.value))}></input></>
-      }
-      <button onClick={() => setOnGame(!onGame)}>{onGame ? "End Game" : "Start Game"}</button>
+      {onGame && (
+        <>
+          <Mgba gameUrl="/game.gba" volume={volume} />
+          <input
+            type="range"
+            value={volume}
+            min="0"
+            max="1"
+            step="0.05"
+            onChange={(e) => setVolume(Number(e.target.value))}
+          ></input>
+        </>
+      )}
+      <button onClick={() => setOnGame(!onGame)}>
+        {onGame ? "End Game" : "Start Game"}
+      </button>
     </div>
   );
 }
--- website/src/mgba.tsx
+++ website/src/mgba.tsx
@@ -4,94 +4,102 @@ import mGBA from "./vendor/mgba";
 type Module = any;
 
 interface MgbaProps {
-    gameUrl: string,
-    volume?: Number,
+  gameUrl: string;
+  volume?: Number;
 }
 
 enum MgbaState {
-    Uninitialised,
-    Initialising,
-    Initialised,
+  Uninitialised,
+  Initialising,
+  Initialised,
 }
 
 const MGBA_ROM_DIRECTORY = "/data/games";
 
-
 export const Mgba: FC<MgbaProps> = ({ gameUrl, volume }) => {
+  const canvas = useRef(null);
+  const mgbaModule = useRef<Module>({});
 
-    const canvas = useRef(null);
-    const mgbaModule = useRef<Module>({});
+  const [state, setState] = useState(MgbaState.Uninitialised);
 
-    const [state, setState] = useState(MgbaState.Uninitialised);
+  useEffect(() => {
+    if (state !== MgbaState.Initialised) return;
+    (async () => {
+      const game = await fetch(gameUrl);
+      const gameData = await game.arrayBuffer();
 
-    useEffect(() => {
-        if (state !== MgbaState.Initialised) return;
-        (async () => {
-            const game = await fetch(gameUrl);
-            const gameData = await game.arrayBuffer();
+      const gamePath = `${MGBA_ROM_DIRECTORY}/${gameUrl}`;
+      mgbaModule.current.FS.writeFile(gamePath, new Uint8Array(gameData));
+      mgbaModule.current.loadGame(gamePath);
+    })();
+  }, [state, gameUrl]);
 
-            const gamePath = `${MGBA_ROM_DIRECTORY}/${gameUrl}`;
-            mgbaModule.current.FS.writeFile(gamePath, new Uint8Array(gameData));
-            mgbaModule.current.loadGame(gamePath);
-        })()
-    }, [state, gameUrl]);
+  // init mgba
+  useEffect(() => {
+    (async () => {
+      if (canvas === null) return;
+      if (state !== MgbaState.Uninitialised) return;
 
-    // init mgba
-    useEffect(() => {
-        (async () => {
+      setState(MgbaState.Initialising);
 
-            if (canvas === null) return;
-            if (state !== MgbaState.Uninitialised) return;
+      mgbaModule.current = {
+        canvas: canvas.current,
+        locateFile: (file: string) => {
+          if (file === "mgba.wasm") {
+            return "/vendor/mgba.wasm";
+          }
+          return file;
+        },
+      };
 
-            setState(MgbaState.Initialising);
+      mGBA(mgbaModule.current).then((module: Module) => {
+        mgbaModule.current = module;
+        module.FSInit();
+        setState(MgbaState.Initialised);
+      });
+    })();
 
-            mgbaModule.current = {
-                canvas: canvas.current,
-                locateFile: (file: string) => {
-                    if (file === "mgba.wasm") {
-                        return "/vendor/mgba.wasm";
-                    }
-                    return file;
-                }
-            };
+    if (state === MgbaState.Initialised)
+      return () => {
+        try {
+          mgbaModule.current.quitGame();
+          mgbaModule.current.quitMgba();
+        } catch {}
+      };
+  }, [state]);
 
-            mGBA(mgbaModule.current).then((module: Module) => {
-                mgbaModule.current = module;
-                module.FSInit();
-                setState(MgbaState.Initialised);
-            });
-        }
-        )();
+  useEffect(() => {
+    if (state !== MgbaState.Initialised) return;
+    mgbaModule.current.setVolume(volume ?? 1.0);
+  }, [state, volume]);
 
-        if (state === MgbaState.Initialised)
-            return () => {
-                try {
-                    mgbaModule.current.quitGame();
-                    mgbaModule.current.quitMgba();
-                } catch { }
-            };
-    }, [state]);
-
-    useEffect(() => {
-        if (state !== MgbaState.Initialised) return;
-        mgbaModule.current.setVolume(volume ?? 1.0);
-    }, [state, volume]);
-
-    return <>
-
-        <canvas ref={canvas}></canvas>
-        <button onClick={() => {
-            if (state !== MgbaState.Initialised) return;
-            mgbaModule.current.saveState(0);
-        }}>Save State</button>
-        <button onClick={() => {
-            if (state !== MgbaState.Initialised) return;
-            mgbaModule.current.loadState(0);
-        }}>Load State</button>
-        <button onClick={() => {
-            if (state !== MgbaState.Initialised) return;
-            mgbaModule.current.quickReload();
-        }}>Restart</button>
-
-    </>;
+  return (
+    <>
+      <canvas ref={canvas}></canvas>
+      <button
+        onClick={() => {
+          if (state !== MgbaState.Initialised) return;
+          mgbaModule.current.saveState(0);
+        }}
+      >
+        Save State
+      </button>
+      <button
+        onClick={() => {
+          if (state !== MgbaState.Initialised) return;
+          mgbaModule.current.loadState(0);
+        }}
+      >
+        Load State
+      </button>
+      <button
+        onClick={() => {
+          if (state !== MgbaState.Initialised) return;
+          mgbaModule.current.quickReload();
+        }}
+      >
+        Restart
+      </button>
+    </>
+  );
 };
