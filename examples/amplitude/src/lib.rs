929eef1569ad5f4d2dc48429eaaa6ab5c5663878
Corwin <corwin@kuiper.dev>
2023-04-25 21:01:04 +0100

    add deny

--- examples/amplitude/src/lib.rs
+++ examples/amplitude/src/lib.rs
@@ -3,6 +3,7 @@
 #![cfg_attr(test, feature(custom_test_frameworks))]
 #![cfg_attr(test, reexport_test_harness_main = "test_main")]
 #![cfg_attr(test, test_runner(agb::test_runner::test_runner))]
+#![deny(clippy::all)]
 
 extern crate alloc;
 
