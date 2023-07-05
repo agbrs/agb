baab2582b5102b51f546daa0ebd51d6d6fb12529
Gwilym Inzani <email@gwilym.dev>
2023-07-05 10:07:10 +0100

    Play the game by default

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -3,34 +3,27 @@ import { Mgba } from "./mgba";
 import { BindingsControl, DefaultBindingsSet } from "./bindings";
 
 function App() {
-  const [onGame, setOnGame] = useState(false);
   const [volume, setVolume] = useState(1.0);
   const [bindings, setBindings] = useState(DefaultBindingsSet());
   const [paused, setPaused] = useState(false);
 
   return (
     <div>
-      {onGame && (
-        <>
-          <Mgba
-            gameUrl="/game.gba"
-            volume={volume}
-            controls={bindings.Actual}
-            paused={paused}
-          />
-          <input
-            type="range"
-            value={volume}
-            min="0"
-            max="1"
-            step="0.05"
-            onChange={(e) => setVolume(Number(e.target.value))}
-          ></input>
-        </>
-      )}
-      <button onClick={() => setOnGame(!onGame)}>
-        {onGame ? "End Game" : "Start Game"}
-      </button>
+      <Mgba
+        gameUrl="/game.gba"
+        volume={volume}
+        controls={bindings.Actual}
+        paused={paused}
+      />
+      <input
+        type="range"
+        value={volume}
+        min="0"
+        max="1"
+        step="0.05"
+        onChange={(e) => setVolume(Number(e.target.value))}
+      ></input>
+
       <BindingsControl
         bindings={bindings}
         setBindings={setBindings}
