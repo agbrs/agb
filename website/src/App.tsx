064a919bd6c874054f256c763f6a584f498c3cf8
Corwin <corwin@kuiper.dev>
2023-07-05 21:47:52 +0100

    handle overflow of dialog

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -10,6 +10,8 @@ import { Slider } from "./Slider";
 const BindingsDialog = styled.dialog`
   border-radius: 5px;
   margin-top: 20px;
+  overflow-y: scroll;
+  max-height: calc(100vh - 100px);
 `;
 
 const VolumeLabel = styled.label`
