b8ceb26bff7eb543999c28ccaa96ec2044a44e3f
Gwilym Inzani <email@gwilym.dev>
2023-08-06 21:06:29 +0100

    Also only git add if passed --commit

--- .github/scripts/update-lockfiles.sh
+++ .github/scripts/update-lockfiles.sh
@@ -9,14 +9,14 @@ update_lockfiles
 update_lockfiles
 update_lockfiles
 
-git add -u
-
-if [ "$(git diff --cached --name-only)" == '' ]; then
-    echo "No files updated"
-    exit 0
-fi
-
 if [ "$1" == '--commit' ]; then
+    git add -u
+
+    if [ "$(git diff --cached --name-only)" == '' ]; then
+        echo "No files updated"
+        exit 0
+    fi
+
     git -c user.name="GBA bot" -c user.email="gw@ilym.me" commit -m 'Update lockfiles'
     git push
 fi
