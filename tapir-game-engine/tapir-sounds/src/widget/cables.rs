35211be66f500e81cb96fbb9161c5e0720231368
Gwilym Inzani <email@gwilym.dev>
2023-08-12 13:56:54 +0100

    Paint cables on top

--- tapir-game-engine/tapir-sounds/src/widget/cables.rs
+++ tapir-game-engine/tapir-sounds/src/widget/cables.rs
@@ -3,28 +3,35 @@ use eframe::egui;
 use crate::widget;
 
 pub fn cables(ui: &mut egui::Ui, cables: impl Iterator<Item = (widget::PortId, widget::PortId)>) {
-    let painter = ui.painter();
+    ui.with_layer_id(
+        egui::LayerId::new(egui::Order::Tooltip, egui::Id::new("cables")),
+        |ui| {
+            let painter = ui.painter();
 
-    let cable_stroke = ui.style().visuals.window_stroke();
+            let cable_stroke = ui.style().visuals.window_stroke();
 
-    for (source, target) in cables {
-        let Some((source_pos, target_pos)) = widget::CableState::from_ctx(ui.ctx(), |state| {
-            Some((
-                state.get_port_position(&source)?,
-                state.get_port_position(&target)?,
-            ))
-        }) else {
-            continue;
-        };
+            for (source, target) in cables {
+                let Some((source_pos, target_pos)) =
+                    widget::CableState::from_ctx(ui.ctx(), |state| {
+                        Some((
+                            state.get_port_position(&source)?,
+                            state.get_port_position(&target)?,
+                        ))
+                    })
+                else {
+                    continue;
+                };
 
-        painter.line_segment([source_pos, target_pos], cable_stroke);
-    }
+                painter.line_segment([source_pos, target_pos], cable_stroke);
+            }
 
-    if let Some(in_progress_cable_pos) =
-        widget::CableState::from_ctx(ui.ctx(), |state| state.in_progress_cable_pos())
-    {
-        if let Some(cursor_pos) = ui.ctx().input(|i| i.pointer.interact_pos()) {
-            painter.line_segment([in_progress_cable_pos, cursor_pos], cable_stroke);
-        }
-    }
+            if let Some(in_progress_cable_pos) =
+                widget::CableState::from_ctx(ui.ctx(), |state| state.in_progress_cable_pos())
+            {
+                if let Some(cursor_pos) = ui.ctx().input(|i| i.pointer.interact_pos()) {
+                    painter.line_segment([in_progress_cable_pos, cursor_pos], cable_stroke);
+                }
+            }
+        },
+    );
 }
