46f5e99868706be0ec6dafc9d9bbb1b8f5eeef9b
Gwilym Inzani <email@gwilym.dev>
2023-08-13 19:13:36 +0100

    Add offset to fade

--- tapir-game-engine/tapir-sounds/src/state/blocks/fade.rs
+++ tapir-game-engine/tapir-sounds/src/state/blocks/fade.rs
@@ -5,11 +5,15 @@ use super::BlockType;
 #[derive(Clone)]
 pub struct Fade {
     amplitude: f64,
+    offset: f64,
 }
 
 impl Default for Fade {
     fn default() -> Self {
-        Self { amplitude: 1.0 }
+        Self {
+            amplitude: 1.0,
+            offset: 0.0,
+        }
     }
 }
 
@@ -28,13 +32,19 @@ impl BlockType for Fade {
     }
 
     fn inputs(&self) -> Vec<(Cow<'static, str>, super::Input)> {
-        vec![("Amplitude".into(), super::Input::Amplitude(self.amplitude))]
+        vec![
+            ("Amplitude".into(), super::Input::Amplitude(self.amplitude)),
+            ("Offset".into(), super::Input::Periods(self.offset)),
+        ]
     }
 
     fn set_input(&mut self, index: usize, value: &super::Input) {
         match (index, value) {
             (0, super::Input::Amplitude(new_amplitude)) => {
                 self.amplitude = *new_amplitude;
+            }
+            (1, super::Input::Periods(new_offset)) => {
+                self.offset = new_offset.clamp(0.0, 1.0);
             }
             _ => panic!("Invalid input {index} {value:?}"),
         }
@@ -43,26 +53,37 @@ impl BlockType for Fade {
     fn calculate(&self, _global_frequency: f64, inputs: &[Option<&[f64]>]) -> Vec<f64> {
         let input = inputs[0].unwrap_or_default();
 
-        let length = input.len() as f64;
+        let length = input.len() as f64 * (1.0 - self.offset);
+        let offset_start = (input.len() as f64 * self.offset) as usize;
 
         if self.amplitude > 0.0 {
-            // start at amplitude and end at 0
+            // start at amplitude and end at 0. But only start doing something once we get to offset
             input
                 .iter()
                 .enumerate()
                 .map(|(i, value)| {
-                    let amount = self.amplitude * (1.0 - i as f64 / length);
-                    *value * amount
+                    if i < offset_start {
+                        *value * self.amplitude
+                    } else {
+                        let amount = self.amplitude * (1.0 - (i - offset_start) as f64 / length);
+                        *value * amount
+                    }
                 })
                 .collect()
         } else {
-            // start at 0 and end at -amplitude
+            let offset_start = input.len() - offset_start;
+
+            // start at 0 and end at -amplitude. But only start doing something once we get to offset
             input
                 .iter()
                 .enumerate()
                 .map(|(i, value)| {
-                    let amount = -self.amplitude * (i as f64 / length);
-                    *value * amount
+                    if i > offset_start {
+                        *value * -self.amplitude
+                    } else {
+                        let amount = -self.amplitude * (i as f64 / length);
+                        *value * amount
+                    }
                 })
                 .collect()
         }
