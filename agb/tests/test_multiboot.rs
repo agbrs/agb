ce233f7d6265e91a1a9a2a05d649525e367617a9
Gwilym Inzani <email@gwilym.dev>
2023-10-17 23:51:40 +0100

    Always check the address to make sure that things work as expected

--- agb/tests/test_multiboot.rs
+++ agb/tests/test_multiboot.rs
@@ -8,11 +8,17 @@ fn hello() {}
 
 #[test_case]
 fn multiboot_test(_gba: &mut agb::Gba) {
+    let address: usize = hello as usize;
+
     if option_env!("AGB_MULTIBOOT").is_some() {
-        let address = hello as usize;
         assert!(
             (0x0200_0000..0x0204_0000).contains(&address),
             "multiboot functions should all be in ewram 0x0300_0000 and 0x0300_8000, but was actually found to be at {address:#010X}"
+        );
+    } else {
+        assert!(
+            address >= 0x0800_0000,
+            "functions should all be in ROM >= 0x0800_0000, but was actually found to be at {address:#010X}"
         );
     }
 }
