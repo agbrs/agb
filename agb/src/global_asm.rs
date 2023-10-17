7afa19965c419a4977d1209137af9bb5e9b0b9b5
Gwilym Inzani <email@gwilym.dev>
2023-10-17 22:12:50 +0100

    Rename to entrypoint.s

--- agb/src/global_asm.rs
+++ agb/src/global_asm.rs
@@ -2,7 +2,7 @@ use core::arch::global_asm;
 
 global_asm!(include_str!("asm_include.s"));
 
-global_asm!(include_str!("crt0.s"));
+global_asm!(include_str!("entrypoint.s"));
 global_asm!(include_str!("interrupt_handler.s"));
 global_asm!(include_str!("sound/mixer/mixer.s"));
 global_asm!(include_str!("save/asm_routines.s"));
