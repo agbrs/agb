f66b27e27911a449d8a4eccfe02f545b43219f81
Gwilym Inzani <email@gwilym.dev>
2025-01-22 16:01:14 +0000

    Fix a few more examples

--- agb/examples/test_logo.rs
+++ agb/examples/test_logo.rs
@@ -1,14 +1,17 @@
 #![no_std]
 #![no_main]
 
-use agb::display::{
-    example_logo,
-    tiled::{RegularBackgroundSize, RegularBackgroundTiles, TileFormat},
+use agb::{
+    display::{
+        example_logo,
+        tiled::{RegularBackgroundSize, RegularBackgroundTiles, TileFormat},
+    },
+    syscall,
 };
 
 #[agb::entry]
 fn main(mut gba: agb::Gba) -> ! {
-    let (mut gfx, mut vram) = gba.display.video.tiled();
+    let mut gfx = gba.display.video.tiled();
 
     let mut map = RegularBackgroundTiles::new(
         agb::display::Priority::P0,
@@ -16,12 +19,14 @@ fn main(mut gba: agb::Gba) -> ! {
         TileFormat::FourBpp,
     );
 
-    example_logo::display_logo(&mut map, &mut vram);
+    example_logo::display_logo(&mut map);
     map.commit();
 
     let mut bg_iter = gfx.iter();
     map.show(&mut bg_iter);
-    bg_iter.commit(&mut vram);
+    bg_iter.commit();
 
-    loop {}
+    loop {
+        syscall::halt();
+    }
 }
