e4a799c9da673e27b6f5b20b4e0edc0c83a4e3fe
Gwilym Inzani <email@gwilym.dev>
2025-01-28 22:20:55 +0000

    Make background iter inclusive (again)

--- agb-fixnum/src/lib.rs
+++ agb-fixnum/src/lib.rs
@@ -1035,22 +1035,22 @@ impl<T: Number> Rect<T> {
 impl<T: FixedWidthUnsignedInteger> Rect<T> {
     /// Iterate over the points in a rectangle in row major order.
     /// ```
-    /// # use agb_fixnum::*;
-    /// let r = Rect::new(Vector2D::new(1,1), Vector2D::new(2,3));
+    /// use agb_fixnum::{Rect, vec2};
+    /// let r = Rect::new(vec2(1,1), vec2(1,2));
     ///
-    /// let expected_points = vec![(1,1), (2,1), (1,2), (2,2), (1,3), (2,3)];
-    /// let rect_points: Vec<(i32, i32)> = r.iter().collect();
+    /// let expected_points = vec![vec2(1,1), vec2(2,1), vec2(1,2), vec2(2,2), vec2(1,3), vec2(2,3)];
+    /// let rect_points: Vec<_> = r.iter().collect();
     ///
     /// assert_eq!(rect_points, expected_points);
     /// ```
-    pub fn iter(self) -> impl Iterator<Item = (T, T)> {
+    pub fn iter(self) -> impl Iterator<Item = Vector2D<T>> {
         let mut x = self.position.x;
         let mut y = self.position.y;
         core::iter::from_fn(move || {
-            if x >= self.position.x + self.size.x {
+            if x > self.position.x + self.size.x {
                 x = self.position.x;
                 y = y + T::one();
-                if y >= self.position.y + self.size.y {
+                if y > self.position.y + self.size.y {
                     return None;
                 }
             }
@@ -1058,7 +1058,7 @@ impl<T: FixedWidthUnsignedInteger> Rect<T> {
             let ret_x = x;
             x = x + T::one();
 
-            Some((ret_x, y))
+            Some(vec2(ret_x, y))
         })
     }
 }
@@ -1500,19 +1500,19 @@ mod tests {
 
     #[test]
     fn test_rect_iter() {
-        let rect: Rect<i32> = Rect::new((5_i32, 5_i32).into(), (3_i32, 3_i32).into());
+        let rect: Rect<i32> = Rect::new((5_i32, 5_i32).into(), (2_i32, 2_i32).into());
         assert_eq!(
             rect.iter().collect::<alloc::vec::Vec<_>>(),
             &[
-                (5, 5),
-                (6, 5),
-                (7, 5),
-                (5, 6),
-                (6, 6),
-                (7, 6),
-                (5, 7),
-                (6, 7),
-                (7, 7),
+                vec2(5, 5),
+                vec2(6, 5),
+                vec2(7, 5),
+                vec2(5, 6),
+                vec2(6, 6),
+                vec2(7, 6),
+                vec2(5, 7),
+                vec2(6, 7),
+                vec2(7, 7),
             ]
         );
     }
