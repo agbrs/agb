110dee9252ac074aebe0c3d6a7ef0342d7a58dd4
Gwilym Inzani <email@gwilym.dev>
2025-02-06 20:59:28 +0000

    Hello axum

--- website/play/src/main.rs
+++ website/play/src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{response::Html, routing::get, Router};
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(handler));
+
+    let listener = TcpListener::bind("127.0.0.1:3000").await.unwrap();
+    println!("listening on {}", listener.local_addr().unwrap());
+
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn handler() -> Html<&'static str> {
+    Html("<h1>Hello, World!</h1>")
 }
