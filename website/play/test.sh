14d7fee06d1b8a5351524a29cdfcf8d0ab3874bd
Gwilym Inzani <email@gwilym.dev>
2025-02-06 23:40:48 +0000

    Better test script

--- website/play/test.sh
+++ website/play/test.sh
@@ -2,7 +2,12 @@
 
 set -euo pipefail
 
-curl http://localhost:3000/build --json @test.json --output out.gba.gz
+if [ "$#" != 1 ]; then
+    echo "Usage: $0 <rust file to compile>"
+    exit 1
+fi
+
+curl -v http://localhost:3000/build --json "$(jq -Rs '{code: .}' < "$1")" --output out.gba.gz
 if gunzip out.gba.gz; then
     mgba-qt out.gba
     rm -f out.gba out.gba.sav
