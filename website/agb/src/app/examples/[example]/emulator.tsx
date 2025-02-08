c4a412cc40dfdf0f5311edb7315fd81f2f0c9757
Corwin <corwin@kuiper.dev>
2025-02-08 13:09:05 +0000

    simple display of the log messages

--- website/agb/src/app/examples/[example]/emulator.tsx
+++ website/agb/src/app/examples/[example]/emulator.tsx
@@ -3,7 +3,8 @@
 import MgbaWrapper from "@/components/mgba/mgbaWrapper";
 import { Examples } from "@/roms/examples/examples";
 import { slugify } from "@/sluggify";
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
+import styled from "styled-components";
 
 function gameUrl(exampleName: string) {
   const example = Examples.find((x) => slugify(x.example_name) === exampleName);
@@ -14,8 +15,35 @@ function gameUrl(exampleName: string) {
   return example.url;
 }
 
+const LogList = styled.ol`
+  max-height: 400px;
+  overflow-y: scroll;
+`;
+
+function LogDisplay({ messages }: { messages: string[] }) {
+  return (
+    <LogList>
+      {messages.map((x, idx) => (
+        <li key={idx}>{x}</li>
+      ))}
+    </LogList>
+  );
+}
+
 export function Emulator({ exampleName }: { exampleName: string }) {
   const example = useMemo(() => gameUrl(exampleName), [exampleName]);
+  const [logs, setLogs] = useState<string[]>([]);
 
-  return <MgbaWrapper gameUrl={example} />;
+  return (
+    <>
+      <MgbaWrapper
+        gameUrl={example}
+        onLogMessage={(category, level, message) => {
+          if (category === "GBA BIOS") return;
+          setLogs((logs) => [...logs, `${category} ${level} ${message}`]);
+        }}
+      />
+      <LogDisplay messages={logs} />
+    </>
+  );
 }
