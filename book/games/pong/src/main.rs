9907687a677194b553062baa30d7a3af8e97af98
Gwilym Inzani <email@gwilym.dev>
2025-03-15 15:27:26 +0000

    Remove redundant part of the comment

--- book/games/pong/src/main.rs
+++ book/games/pong/src/main.rs
@@ -1,9 +1,6 @@
 // Games made using `agb` are no_std which means you don't have access to the standard
 // rust library. This is because the game boy advance doesn't really have an operating
 // system, so most of the content of the standard library doesn't apply.
-//
-// Provided you haven't disabled it, agb does provide an allocator, so it is possible
-// to use both the `core` and the `alloc` built in crates.
 #![no_std]
 // `agb` defines its own `main` function, so you must declare your game's main function
 // using the #[agb::entry] proc macro. Failing to do so will cause failure in linking
