9fe3d8ac0a5d3068d44f23f0ae8ba683e20a4e18
Gwilym Inzani <email@gwilym.dev>
2025-04-12 23:27:21 +0100

    Add doc comments about colours

--- agb/src/display/colours.rs
+++ agb/src/display/colours.rs
@@ -1,18 +1,25 @@
+#![warn(missing_docs)]
 use core::fmt::Debug;
 
 use crate::{fixnum::Num, fixnum::num};
 
+/// Represents a pixel on the GBA. This is stored as a 15 bit number as `0b0bbbbbgggggrrrrr`. You can see
+/// what would happen to your true-colour value by using the [utility site](https://agbrs.dev/colour) in the agbrs.dev website.
 #[repr(transparent)]
 #[derive(Clone, Copy, PartialEq, Eq)]
 pub struct Rgb15(pub u16);
 
 impl Rgb15 {
+    /// Creates a new Rgb15 value. You should probably use `Rgb15(<your number>)` rather than this constructor,
+    /// but it exists to use as a function reference when that is useful.
     #[must_use]
     pub const fn new(value: u16) -> Self {
         Self(value)
     }
 
+    /// A black Rgb15 value
     pub const BLACK: Rgb15 = Rgb::new(0, 0, 0).to_rgb15();
+    /// A white Rgb15 value
     pub const WHITE: Rgb15 = Rgb::new(255, 255, 255).to_rgb15();
 }
 
@@ -29,19 +36,26 @@ impl Debug for Rgb15 {
     }
 }
 
+/// Represents a full true-colour (24-bit) RGB colour. You can convert (lossily) between this and
+/// [`Rgb15`] values which actually get displayed on the GBA screen.
 #[derive(Clone, Copy, PartialEq, Eq, Hash)]
 pub struct Rgb {
+    /// The red component
     pub r: u8,
+    /// The green component
     pub g: u8,
+    /// The blue component
     pub b: u8,
 }
 
 impl Rgb {
+    /// Create a new Rgb value with given red, green and blue components
     #[must_use]
     pub const fn new(r: u8, g: u8, b: u8) -> Self {
         Self { r, g, b }
     }
 
+    /// Create from an [`Rgb15`] value. This exists in addition to the `From` implementation because it is `const`.
     #[must_use]
     pub const fn from_rgb15(rgb15: Rgb15) -> Self {
         let rgb15 = rgb15.0;
@@ -52,12 +66,15 @@ impl Rgb {
         Self::new(r as u8, g as u8, b as u8)
     }
 
+    /// Create an [`Rgb15`] value. This exists in addition to the `From` implementation because it is `const`.
     #[must_use]
     pub const fn to_rgb15(self) -> Rgb15 {
         let (r, g, b) = (self.r as u16, self.g as u16, self.b as u16);
         Rgb15(((r >> 3) & 31) | (((g >> 3) & 31) << 5) | (((b >> 3) & 31) << 10))
     }
 
+    /// Interpolate between self and another colour. This does a simple linear interpolation. Amount should be
+    /// between 0 and 1 (inclusive), but could technically extend if you want to overshoot.
     #[must_use]
     pub fn interpolate(self, other: Self, amount: Num<i32, 8>) -> Self {
         let inv_amount = num!(1.) - amount;
