5e8248a6144958a534f348cb85ddfd9a1e0c5806
Corwin <corwin@kuiper.dev>
2025-04-14 22:00:03 +0100

    animation sprite iterator

--- agb/src/display/object/sprites.rs
+++ agb/src/display/object/sprites.rs
@@ -4,7 +4,7 @@ mod sprite_allocator;
 const BYTES_PER_TILE_4BPP: usize = 32;
 const BYTES_PER_TILE_8BPP: usize = 16;
 
-pub use sprite::{PaletteMulti, Size, Sprite, Tag, include_aseprite};
+pub use sprite::{AnimationIterator, PaletteMulti, Size, Sprite, Tag, include_aseprite};
 pub use sprite_allocator::{
     DynamicSprite16, DynamicSprite256, PaletteVram, PaletteVramMulti, PaletteVramSingle,
     SPRITE_LOADER, SpriteVram,
