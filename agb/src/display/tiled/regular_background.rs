7754d840ec1d8d04d85582f7c21fee8b9e2ee78d
Gwilym Inzani <email@gwilym.dev>
2025-04-14 22:49:52 +0100

    Include an example too

--- agb/src/display/tiled/regular_background.rs
+++ agb/src/display/tiled/regular_background.rs
@@ -232,6 +232,29 @@ impl RegularBackgroundTiles {
     ///
     /// This method assumes that `tile_data` was loaded via [`include_background_gfx!`](crate::include_background_gfx) and
     /// that it is exactly the same size as the Game Boy Advance's screen resolution of 240x160 pixels (or 20x30 tiles).
+    ///
+    /// ## Example
+    ///
+    /// ```rust,no_run
+    /// # #![no_main]
+    /// # #![no_std]
+    /// use agb::{
+    ///     display::{
+    ///         Priority,
+    ///         tiled::{RegularBackgroundTiles, RegularBackgroundSize, TileFormat, VRAM_MANAGER},
+    ///     },
+    ///     include_background_gfx,
+    /// };
+    ///
+    /// include_background_gfx!(logo, logo => deduplicate "gfx/test_logo.png");
+    ///
+    /// # fn foo() {
+    /// VRAM_MANAGER.set_background_palettes(logo::PALETTES);
+    /// let mut bg = RegularBackgroundTiles::new(Priority::P0, RegularBackgroundSize::Background32x32, TileFormat::FourBpp);
+    ///
+    /// bg.fill_with(&logo::logo);
+    /// # }
+    /// ```
     pub fn fill_with(&mut self, tile_data: &TileData) {
         assert!(
             tile_data.tile_settings.len() >= 20 * 30,
