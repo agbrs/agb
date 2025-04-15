50c326ae35733ce86d617b900553b2664a061c5f
Corwin <corwin@kuiper.dev>
2025-04-15 21:30:06 +0100

    extend chicken world

--- agb/examples/chicken.rs
+++ agb/examples/chicken.rs
@@ -5,7 +5,10 @@ use agb::{
     display::{
         GraphicsFrame, HEIGHT, WIDTH,
         object::{Object, Sprite},
-        tiled::{RegularBackgroundSize, RegularBackgroundTiles, TileFormat, VRAM_MANAGER},
+        tiled::{
+            InfiniteScrolledMap, RegularBackgroundSize, RegularBackgroundTiles, TileFormat,
+            VRAM_MANAGER,
+        },
     },
     fixnum::{Num, Rect, Vector2D, num, vec2},
     include_aseprite, include_background_gfx,
@@ -39,16 +42,16 @@ struct Chicken {
 }
 
 fn tile_is_collidable(tile: Vector2D<i32>) -> bool {
-    if tile.x < 0 || tile.x > 32 || tile.y < 0 || tile.y > 32 {
+    if tile.x < 0 || tile.x > 30 * 5 || tile.y < 0 || tile.y > 32 {
         true
     } else {
-        let idx = tile.x + tile.y * 32;
+        let idx = tile.x + tile.y * 30 * 5;
         let tile = background::map.tile_settings[idx as usize].tile_id();
 
         // I just grabbed the indexes of the 2 collidable tiles from aseprite
         const COLLIDABLE_TILES: &[u16] = &[
-            background::map.tile_settings[227].tile_id(),
-            background::map.tile_settings[355].tile_id(),
+            background::map.tile_settings[1053].tile_id(),
+            background::map.tile_settings[1653].tile_id(),
         ];
 
         COLLIDABLE_TILES.contains(&tile)
@@ -56,24 +59,27 @@ fn tile_is_collidable(tile: Vector2D<i32>) -> bool {
 }
 
 #[agb::entry]
-fn main(mut gba: agb::Gba) -> ! {
+fn main(gba: agb::Gba) -> ! {
+    entry(gba);
+}
+
+fn entry(mut gba: agb::Gba) -> ! {
     let mut gfx = gba.graphics.get();
     let mut input = agb::input::ButtonController::new();
 
     VRAM_MANAGER.set_background_palettes(background::PALETTES);
 
-    let mut background = RegularBackgroundTiles::new(
+    let background = RegularBackgroundTiles::new(
         agb::display::Priority::P0,
         RegularBackgroundSize::Background32x32,
         TileFormat::FourBpp,
     );
 
-    for (i, &tile) in background::map.tile_settings.iter().enumerate() {
-        let i = i as u16;
-        background.set_tile((i % 32, i / 32), &background::map.tiles, tile);
-    }
+    let mut background = InfiniteScrolledMap::new(background);
 
     let mut chicken = Chicken::new(vec2(6, 7));
+
+    let mut camera_position: Vector = (0, 0).into();
 
     let mut frame_count = 0usize;
 
@@ -82,9 +88,29 @@ fn main(mut gba: agb::Gba) -> ! {
 
         input.update();
 
+        let target_position = chicken.position.x.floor() / WIDTH * WIDTH;
+        camera_position.x += (Number::new(target_position) - camera_position.x) / 8;
+
+        background.set_scroll_pos(
+            vec2(camera_position.x.round(), camera_position.y.floor()),
+            |pos| {
+                let (x, y) = (pos.x, pos.y);
+                let tile_idx = if !(0..30 * 5).contains(&x) || !(0..20).contains(&y) {
+                    0
+                } else {
+                    x as usize + y as usize * 30 * 5
+                };
+
+                (
+                    &background::map.tiles,
+                    background::map.tile_settings[tile_idx],
+                )
+            },
+        );
+
         let mut frame = gfx.frame();
         background.show(&mut frame);
-        chicken.update(frame_count, &input, &mut frame);
+        chicken.update(frame_count, &input, camera_position, &mut frame);
 
         frame.commit();
     }
@@ -101,7 +127,13 @@ impl Chicken {
         }
     }
 
-    fn update(&mut self, frame_count: usize, input: &ButtonController, frame: &mut GraphicsFrame) {
+    fn update(
+        &mut self,
+        frame_count: usize,
+        input: &ButtonController,
+        camera_position: Vector,
+        frame: &mut GraphicsFrame,
+    ) {
         if self.state != State::Ground {
             self.left_ground_frames += 1;
         } else {
@@ -118,11 +150,11 @@ impl Chicken {
 
         self.restrict_to_screen();
         self.update_collision();
-        self.update_sprite(frame_count);
+        self.update_sprite(frame_count, camera_position);
         self.object.show(frame);
     }
 
-    fn update_sprite(&mut self, frame: usize) {
+    fn update_sprite(&mut self, frame: usize, camera_position: Vector) {
         if self.velocity.x.to_raw() > 1 {
             self.object.set_hflip(false);
         } else if self.velocity.x.to_raw() < -1 {
@@ -144,16 +176,23 @@ impl Chicken {
         }
 
         self.object
-            .set_position((self.position + vec2(num!(0.5), num!(0.5))).floor() - vec2(4, 4));
+            .set_position((self.position - camera_position).round() - vec2(4, 4));
     }
 
     fn restrict_to_screen(&mut self) {
         let bounding_rect = Rect::new(
             vec2(num!(4), num!(4)),
-            vec2(num!(WIDTH - 8), num!(HEIGHT - 8)),
+            vec2(num!(WIDTH * 5 - 8), num!(HEIGHT - 8)),
         );
 
+        let previous = self.position;
         self.position = bounding_rect.clamp_point(self.position);
+        if previous.x != self.position.x {
+            self.velocity.x = num!(0);
+        }
+        if previous.y != self.position.y {
+            self.velocity.y = num!(0);
+        }
     }
 
     fn handle_collision_component(
@@ -208,4 +247,4 @@ include_aseprite!(mod sprites, "examples/gfx/chicken.aseprite");
 use sprites::{JUMP, WALK};
 static IDLE: &Sprite = sprites::IDLE.sprite(0);
 
-include_background_gfx!(mod background, map => deduplicate "examples/gfx/chicken-map.aseprite");
+include_background_gfx!(mod background, "7e8dd2", map => deduplicate "examples/gfx/chicken-map.aseprite");
