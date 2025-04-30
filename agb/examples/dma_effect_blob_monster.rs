40ef89b8022d213d852c34a09703421885f9b2ad
Gwilym Inzani <email@gwilym.dev>
2025-04-30 11:17:17 +0100

    Add some comments on the blob monster

--- agb/examples/dma_effect_blob_monster.rs
+++ agb/examples/dma_effect_blob_monster.rs
@@ -1,3 +1,10 @@
+// This example demonstrates a way to use a combination of a background and an object to show a
+// more dynamic object on screen.
+//
+// The monster is made up of a single regular background and a sprite for the eye. You could
+// produce this effect with many large sprites, but with this technique, you could make the outside
+// be effected by the player as well e.g. if they attack a certain part of it, it could
+// mark an indent in that location. This wouldn't be possible with sprites.
 #![no_main]
 #![no_std]
 
@@ -43,11 +50,20 @@ fn main(mut gba: agb::Gba) -> ! {
 }
 
 struct BlobMonster {
+    /// The widths of the individual heights of the blob monster. These are calculated using a
+    /// quartic root to create a pleasing shape for the monster.
     widths: Vec<Num<i32, 12>>,
+    /// The monster needs a mouth. These are the offsets needed to give some animation while the
+    /// monster is breathing.
     mouth_offset: Vec<Num<i32, 12>>,
+    /// The current location of the top middle of the blob monster.
     position: Vector2D<i32>,
+
+    /// The background in use. This is a simple triangle pattern, but we use scroll offset DMA
+    /// to breathe some life into the monster.
     bg: RegularBackgroundTiles,
 
+    /// The current frame, used for animations
     frame: i32,
 }
 
@@ -104,11 +120,12 @@ impl BlobMonster {
     pub fn show(&self, frame: &mut GraphicsFrame) {
         let bg_id = self.bg.show(frame);
 
-        let eye_pos = self.position + vec2(-15, 10);
+        // The eye sprite is a blinking eye animation. It will close and re-open every 128 frames.
         let eye_sprite = sprites::EYE
             .sprites()
             .get((self.frame % 128) as usize)
             .unwrap_or_else(|| sprites::EYE.sprite(0));
+        let eye_pos = self.position + vec2(-15, 10);
 
         Object::new(eye_sprite)
             .set_position(eye_pos)
@@ -122,19 +139,25 @@ impl BlobMonster {
         (0..160)
             .map(|y| {
                 if y < self.position.y {
+                    // If we're higher than the current monster, return 0, 0 which is a blank line
                     vec2(0, 0)
                 } else if y < self.position.y + Self::BLOB_MONSTER_HEIGHT {
+                    // Here we're within the blob monster itself
                     let monster_y = y - self.position.y;
                     let mut width = self.widths[monster_y as usize];
+
+                    // We'd like it to lean forward slightly to give it a bit more life
                     let lean = Num::new(monster_y) / 20;
                     let mut x = Num::new(self.position.x) - width / 2 + lean;
 
+                    // Put a dent in the front to represent the monster's mouth
                     let mouth_width = self.mouth_offset[monster_y as usize];
                     x += mouth_width;
                     width -= mouth_width;
 
                     vec2(-x.trunc() as u16, width.trunc() as u16)
                 } else {
+                    // Here we're below the monster, so just make it a rectangle from here.
                     let max_width = self.widths[Self::BLOB_MONSTER_HEIGHT as usize - 1];
 
                     let x = Num::new(self.position.x) - max_width / 2;
@@ -150,6 +173,8 @@ impl BlobMonster {
     }
 }
 
+// The blob monster background is a simple triangle. We use DMA to control the scroll position
+// in order to show the blob monster as something more than just a single triangle
 fn blob_monster_background() -> RegularBackgroundTiles {
     let mut bg = RegularBackgroundTiles::new(
         Priority::P0,
@@ -162,6 +187,8 @@ fn blob_monster_background() -> RegularBackgroundTiles {
         TileSetting::new(id as u16, TileEffect::default())
     }
 
+    // These slightly awkward tile setups are to draw the triangle given the
+    // 6 tiles we need for it.
     bg.set_tile((0, 0), tileset, tid(0));
     bg.set_tile((0, 1), tileset, tid(2));
     bg.set_tile((1, 1), tileset, tid(3));
