69df16d264291c55f27830c7472de216bceac090
Gwilym Inzani <email@gwilym.dev>
2025-04-30 11:03:00 +0100

    Show the cool monster

--- agb/examples/dma_effect_blob_monster.rs
+++ agb/examples/dma_effect_blob_monster.rs
@@ -12,7 +12,8 @@ use agb::{
     fixnum::{Num, Vector2D, num, vec2},
     include_background_gfx,
 };
-use alloc::vec::Vec;
+
+use alloc::{vec, vec::Vec};
 
 extern crate alloc;
 
@@ -27,38 +28,74 @@ fn main(mut gba: agb::Gba) -> ! {
     let mut gfx = gba.graphics.get();
     let mut monster = BlobMonster::new();
 
-    let mut frame_idx = Num::<i32, 8>::new(0);
-
     loop {
+        monster.update();
+
         let mut frame = gfx.frame();
-        frame_idx += num!(0.01);
 
         monster.show(&mut frame);
-        monster.position =
-            vec2(WIDTH / 2, HEIGHT / 2) + (vec2(frame_idx.sin(), frame_idx.cos()) * 10).trunc();
+
         frame.commit();
     }
 }
 
 struct BlobMonster {
     widths: Vec<Num<i32, 12>>,
+    mouth_offset: Vec<Num<i32, 12>>,
     position: Vector2D<i32>,
     bg: RegularBackgroundTiles,
+
+    frame: i32,
 }
 
 impl BlobMonster {
     const BLOB_MONSTER_HEIGHT: i32 = 90;
 
+    const BLOB_MONSTER_MOUTH_HEIGHT: i32 = 40;
+    const BLOB_MONSTER_MOUTH_SIZE: i32 = 20;
+
     pub fn new() -> Self {
         let blob_monster_widths = (0..Self::BLOB_MONSTER_HEIGHT)
-            .map(|y| Num::new(y).sqrt() * 10)
-            .collect::<Vec<Num<i32, 12>>>();
+            .map(|y| (Num::new(y).sqrt() * 10).sqrt() * 10)
+            .collect();
+
+        let mouth_offset = vec![Num::new(0); 160];
 
         Self {
             widths: blob_monster_widths,
+            mouth_offset,
             bg: blob_monster_background(),
             position: vec2(WIDTH / 2, HEIGHT / 2),
+
+            frame: 0,
         }
+    }
+
+    pub fn update(&mut self) {
+        self.frame = self.frame.wrapping_add(1);
+
+        let frame_frac = Num::<_, 12>::new(self.frame) / 128;
+
+        self.position = vec2(2 * WIDTH / 3, HEIGHT / 2)
+            + (vec2(frame_frac.sin(), frame_frac.cos()) * 10).trunc();
+
+        let mouth_depth = num!(15) - frame_frac.cos() * 5;
+
+        self.mouth_offset = (0..Self::BLOB_MONSTER_HEIGHT)
+            .map(|y| {
+                if y < Self::BLOB_MONSTER_MOUTH_HEIGHT - Self::BLOB_MONSTER_MOUTH_SIZE / 2 {
+                    num!(0)
+                } else if y < Self::BLOB_MONSTER_MOUTH_HEIGHT + Self::BLOB_MONSTER_MOUTH_SIZE / 2 {
+                    let y = Num::new(
+                        y - Self::BLOB_MONSTER_MOUTH_HEIGHT + Self::BLOB_MONSTER_MOUTH_SIZE / 2,
+                    );
+
+                    (num!(1) - (y / Self::BLOB_MONSTER_MOUTH_SIZE).cos()) * mouth_depth
+                } else {
+                    num!(0)
+                }
+            })
+            .collect();
     }
 
     pub fn show(&self, frame: &mut GraphicsFrame) {
@@ -68,8 +105,13 @@ impl BlobMonster {
                     vec2(0, 0)
                 } else if y < self.position.y + Self::BLOB_MONSTER_HEIGHT {
                     let monster_y = y - self.position.y;
-                    let width = self.widths[monster_y as usize];
-                    let x = Num::new(self.position.x) - width / 2;
+                    let mut width = self.widths[monster_y as usize];
+                    let lean = Num::new(monster_y) / 20;
+                    let mut x = Num::new(self.position.x) - width / 2 + lean;
+
+                    let mouth_width = self.mouth_offset[monster_y as usize];
+                    x += mouth_width;
+                    width -= mouth_width;
 
                     vec2(-x.trunc() as u16, width.trunc() as u16)
                 } else {
