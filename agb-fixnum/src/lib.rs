a936c010cb190d2b53f4a0e2f93fd33b491b8ec9
Gwilym Inzani <email@gwilym.dev>
2025-05-04 18:43:34 +0100

    Warn for clippy::all

--- agb-fixnum/src/lib.rs
+++ agb-fixnum/src/lib.rs
@@ -1,6 +1,7 @@
 #![no_std]
 #![deny(missing_docs)]
 #![deny(rustdoc::broken_intra_doc_links)]
+#![warn(clippy::all)]
 //! Fixed point number implementation for representing non integers efficiently.
 //!
 //! If you are using this crate from within `agb`, you should refer to it as `agb::fixnum` rather than `agb_fixnum`.
