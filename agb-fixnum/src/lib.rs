d6219916e726b0e4b4dbeeac708b31cfb87657aa
Gwilym Inzani <email@gwilym.dev>
2025-05-04 18:43:16 +0100

    deny broken_intra_doc_links

--- agb-fixnum/src/lib.rs
+++ agb-fixnum/src/lib.rs
@@ -1,5 +1,6 @@
 #![no_std]
 #![deny(missing_docs)]
+#![deny(rustdoc::broken_intra_doc_links)]
 //! Fixed point number implementation for representing non integers efficiently.
 //!
 //! If you are using this crate from within `agb`, you should refer to it as `agb::fixnum` rather than `agb_fixnum`.
