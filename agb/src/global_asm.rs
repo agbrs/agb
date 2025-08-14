abc0ee7b5c37563eb28e948ca02a7e353cd61757
Corwin <corwin@kuiper.dev>
2025-08-14 08:07:38 +0100

    concat asm files

--- agb/src/global_asm.rs
+++ agb/src/global_asm.rs
@@ -1,8 +1,9 @@
 use core::arch::global_asm;
 
-global_asm!(include_str!("asm_include.s"));
-
-global_asm!(include_str!("entrypoint.s"));
-global_asm!(include_str!("interrupt_handler.s"));
-global_asm!(include_str!("sound/mixer/mixer.s"));
-global_asm!(include_str!("save/asm_routines.s"));
+global_asm!(concat!(
+    include_str!("asm_include.s"),
+    include_str!("entrypoint.s"),
+    include_str!("interrupt_handler.s"),
+    include_str!("sound/mixer/mixer.s"),
+    include_str!("save/asm_routines.s")
+));
