7bdcf3998e3215fdb838fbe4c4126ef92e925cf6
Corwin <corwin@kuiper.dev>
2026-01-04 21:18:11 +0000

    correct DynamicSprite256

--- agb/src/display/object/sprites.rs
+++ agb/src/display/object/sprites.rs
@@ -2,7 +2,7 @@ mod sprite;
 mod sprite_allocator;
 
 const BYTES_PER_TILE_4BPP: usize = 32;
-const BYTES_PER_TILE_8BPP: usize = 16;
+const BYTES_PER_TILE_8BPP: usize = 64;
 
 pub use sprite::{PaletteMulti, Size, Sprite, Tag, include_aseprite};
 pub use sprite_allocator::{
