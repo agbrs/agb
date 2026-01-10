4a7ae1c7e166aea1222eb6a9a5bc6115f63aeae6
Gwilym Inzani <email@gwilym.dev>
2026-01-10 15:30:20 +0000

    Dynamic tiles with different types

--- agb/examples/dynamic_isometric.rs
+++ agb/examples/dynamic_isometric.rs
@@ -14,6 +14,7 @@ use agb::{
     hash_map::HashMap,
     include_background_gfx,
 };
+use alloc::{vec, vec::Vec};
 
 extern crate alloc;
 
@@ -38,6 +39,7 @@ enum TilePosition {
 enum TileType {
     Dirt = 0,
     Water = 1,
+    Air = 2,
 }
 
 fn main(mut gba: Gba) -> ! {
@@ -53,19 +55,14 @@ fn main(mut gba: Gba) -> ! {
 
     let mut tile_cache = TileCache::default();
 
-    for y in 0..16 {
-        for x in 0..8 {
-            let mut index = 0;
-            for pos in TilePosition::iter() {
-                for tile in tile_cache.get_tiles(pos, TileType::Dirt, TileType::Water) {
-                    bg.set_tile_dynamic16(
-                        (x * 4 + (index % 4) as u16, y * 2 + (index / 4) as u16),
-                        tile,
-                        TileEffect::default(),
-                    );
+    let map = Map::new(4, 4);
 
-                    index += 1;
-                }
+    for y in 0..32 {
+        for x in 0..16 {
+            let (position, me, them) = map.get_from_gba_tile(x * 2, y);
+
+            for (i, tile) in tile_cache.get_tiles(position, me, them).iter().enumerate() {
+                bg.set_tile_dynamic16((x * 2 + i as i32, y), tile, TileEffect::default());
             }
         }
     }
@@ -107,17 +104,21 @@ fn build_combined_tile(
 
     for (i, tile) in result.iter_mut().enumerate() {
         let i = i as u16;
-        tile.data().copy_from_slice(
-            tiles::ISOMETRIC
-                .tiles
-                .get_tile_data(i + tile_a as u16 * 4 + position.offset()),
-        );
-        blit_4(
-            tile.data(),
-            tiles::ISOMETRIC
-                .tiles
-                .get_tile_data(i + tile_b as u16 * 4 + position.reverse().offset()),
-        );
+        let me = tiles::ISOMETRIC
+            .tiles
+            .get_tile_data(i + tile_a as u16 * 4 + position.offset());
+        let them = tiles::ISOMETRIC
+            .tiles
+            .get_tile_data(i + tile_b as u16 * 4 + position.reverse().offset());
+
+        let (first, second) = if tile_a <= tile_b {
+            (me, them)
+        } else {
+            (them, me)
+        };
+
+        tile.data().copy_from_slice(first);
+        blit_4(tile.data(), second);
     }
 
     result
@@ -151,4 +152,74 @@ impl TilePosition {
         ]
         .into_iter()
     }
+}
+
+struct Map {
+    map_data: Vec<TileType>,
+    width: usize,
+    height: usize,
+}
+
+const TILE_WIDTH: i32 = 4;
+const TILE_HEIGHT: i32 = 2;
+
+impl Map {
+    fn new(width: usize, height: usize) -> Self {
+        let mut map_data = vec![TileType::Dirt; width * height];
+        map_data[8] = TileType::Water;
+        map_data[9] = TileType::Water;
+        map_data[10] = TileType::Water;
+        map_data[5] = TileType::Air;
+
+        Self {
+            map_data,
+            width,
+            height,
+        }
+    }
+
+    fn get_tile(&self, x: i32, y: i32) -> TileType {
+        if x < 0 || x as usize >= self.width || y < 0 || y as usize >= self.height {
+            return TileType::Air;
+        }
+
+        self.map_data[x as usize + y as usize * self.width]
+    }
+
+    fn get_from_gba_tile(&self, x: i32, y: i32) -> (TilePosition, TileType, TileType) {
+        let x = x - 16;
+        let y = y - 4;
+
+        let tile_position = match ((div_floor(x, 2)).rem_euclid(2), y.rem_euclid(2)) {
+            (0, 0) => TilePosition::TopLeft,
+            (1, 0) => TilePosition::TopRight,
+            (0, 1) => TilePosition::BottomLeft,
+            (1, 1) => TilePosition::BottomRight,
+            _ => unreachable!(),
+        };
+
+        let macro_tile_x = div_floor(x, 4);
+        let macro_tile_y = div_floor(y, 2);
+
+        let (tile_x, tile_y) = (macro_tile_x + macro_tile_y, macro_tile_y - macro_tile_x);
+
+        let neighbour_pos = match tile_position {
+            TilePosition::TopLeft => (-1, 0),
+            TilePosition::TopRight => (0, -1),
+            TilePosition::BottomLeft => (0, 1),
+            TilePosition::BottomRight => (1, 0),
+        };
+
+        let me = self.get_tile(tile_x, tile_y);
+        let neighbour = self.get_tile(tile_x + neighbour_pos.0, tile_y + neighbour_pos.1);
+
+        (tile_position, me, neighbour)
+    }
+}
+
+fn div_floor(numerator: i32, divisor: i32) -> i32 {
+    let d = numerator / divisor;
+    let r = numerator % divisor;
+    let correction = (numerator ^ divisor) >> (i32::BITS - 1);
+    if r != 0 { d + correction } else { d }
 }
