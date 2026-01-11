e679baaf2dab4fde8cf6e2b50d5f5a2cc8cde471
Gwilym Inzani <email@gwilym.dev>
2026-01-11 00:32:23 +0000

    Extract isometric_render.rs

--- examples/dynamic-isometric/src/main.rs
+++ examples/dynamic-isometric/src/main.rs
@@ -69,24 +69,23 @@ use agb::{
     display::{
         GraphicsFrame, Priority, Rgb15,
         object::{GraphicsMode, Object, Tag},
-        tiled::{
-            DynamicTile16, RegularBackground, RegularBackgroundSize, TileEffect, TileFormat,
-            VRAM_MANAGER,
-        },
-        utils::blit_16_colour,
+        tiled::{RegularBackground, RegularBackgroundSize, TileEffect, TileFormat, VRAM_MANAGER},
     },
     dma::HBlankDma,
     fixnum::{Num, Vector2D, num, vec2},
-    hash_map::{HashMap, HashSet},
     include_aseprite, include_background_gfx, include_colours,
     input::ButtonController,
 };
 
-use alloc::{rc::Rc, vec, vec::Vec};
+use alloc::vec;
 
-use core::{array, hash::Hash};
+use isometric_render::TileType;
+
+use crate::isometric_render::{Map, TileCache, world_to_gba_tile_smooth};
 
 extern crate alloc;
+
+mod isometric_render;
 
 include_background_gfx!(mod tiles, "333333",
     ISOMETRIC => "gfx/isometric_tiles.aseprite"
@@ -99,21 +98,6 @@ static SKY_GRADIENT: [Rgb15; 160] = include_colours!("gfx/sky-background-gradien
 #[agb::entry]
 fn entry(gba: Gba) -> ! {
     main(gba);
-}
-
-#[derive(Clone, Copy, Hash, PartialEq, Eq, PartialOrd, Ord, Debug)]
-enum Quadrant {
-    TopLeft,
-    TopRight,
-    BottomLeft,
-    BottomRight,
-}
-
-#[derive(Clone, Copy, Hash, PartialEq, Eq, PartialOrd, Ord, Debug)]
-enum TileType {
-    Dirt = 0,
-    Water = 1,
-    Air = 2,
 }
 
 fn main(mut gba: Gba) -> ! {
@@ -172,13 +156,13 @@ fn main(mut gba: Gba) -> ! {
             let cache_key = floor_map.get_from_gba_tile(x * 2, y);
 
             for (i, tile) in tile_cache.get_tiles(cache_key).iter().enumerate() {
-                floor_bg.set_tile_dynamic16((x * 2 + i as i32, y), &tile.0, TileEffect::default());
+                floor_bg.set_tile_dynamic16((x * 2 + i as i32, y), tile, TileEffect::default());
             }
 
             let cache_key = wall_map.get_from_gba_tile(x * 2, y);
 
             for (i, tile) in tile_cache.get_tiles(cache_key).iter().enumerate() {
-                wall_bg.set_tile_dynamic16((x * 2 + i as i32, y), &tile.0, TileEffect::default());
+                wall_bg.set_tile_dynamic16((x * 2 + i as i32, y), tile, TileEffect::default());
             }
         }
     }
@@ -188,7 +172,7 @@ fn main(mut gba: Gba) -> ! {
 
     let mut input = ButtonController::new();
 
-    agb::println!("Cache size: {}", tile_cache.tiles.len());
+    agb::println!("Cache size: {}", tile_cache.cache_size());
 
     let mut character_target_position = initial_position;
 
@@ -234,258 +218,6 @@ fn main(mut gba: Gba) -> ! {
     }
 }
 
-#[derive(Default)]
-struct TileCache {
-    cache: HashMap<TileSpec, [TileHolder; 2]>,
-    tiles: HashSet<TileHolder>,
-}
-
-#[derive(Clone)]
-struct TileHolder(Rc<DynamicTile16>);
-
-impl Hash for TileHolder {
-    fn hash<H: core::hash::Hasher>(&self, state: &mut H) {
-        self.0.data().hash(state);
-    }
-}
-
-impl PartialEq for TileHolder {
-    fn eq(&self, other: &Self) -> bool {
-        self.0.data() == other.0.data()
-    }
-}
-
-impl Eq for TileHolder {}
-
-#[derive(Clone, Copy, Hash, PartialEq, Eq, Debug)]
-struct TileSpec {
-    quadrant: Quadrant,
-    me: TileType,
-    them: TileType,
-    neighbours: NeighbourTileContext,
-}
-
-// All these refer orthographically to the current tile
-#[derive(Clone, Copy, Hash, PartialEq, Eq, Debug)]
-struct NeighbourTileContext {
-    left: TileType,
-    up_left: TileType,
-    up: TileType,
-    up_right: TileType,
-}
-
-impl TileCache {
-    fn get_tiles(&mut self, cache_key: TileSpec) -> &[TileHolder; 2] {
-        self.cache.entry(cache_key).or_insert_with(|| {
-            let genned_tiles = build_combined_tile(cache_key);
-
-            genned_tiles.map(|genned_tile| {
-                let tile_holder = TileHolder(Rc::new(genned_tile));
-                if let Some(existing_tile) = self.tiles.get(&tile_holder) {
-                    existing_tile.clone()
-                } else {
-                    self.tiles.insert(tile_holder.clone());
-                    tile_holder
-                }
-            })
-        })
-    }
-}
-
-fn build_combined_tile(cache_key: TileSpec) -> [DynamicTile16; 2] {
-    let TileSpec {
-        quadrant,
-        me,
-        them,
-        neighbours,
-    } = cache_key;
-
-    // Wall offset in the tileset (walls are 2 rows below floors)
-    const WALL_OFFSET: u16 = (tiles::ISOMETRIC.width * 2) as u16;
-
-    fn get_tile_id(offset: u16, tile_type: TileType, is_wall: bool) -> u16 {
-        offset + tile_type as u16 * 5 + if is_wall { WALL_OFFSET } else { 0 }
-    }
-
-    // - Upper wall: from a tile above this quadrant on screen (extends down into this quadrant)
-    // - Local wall: from either `me` or `them` (the two tiles this quadrant sits between)
-    let (upper_wall, local_wall, gap_fill) = match quadrant {
-        Quadrant::TopLeft => {
-            // Upper wall: bottom-left wall of up tile
-            // Local wall: top-right wall of left ghost tile
-            let upper = get_tile_id(Quadrant::BottomLeft.offset(), neighbours.up, true);
-            let local = get_tile_id(Quadrant::TopRight.offset(), them, true);
-            (upper, local, None)
-        }
-        Quadrant::TopRight => {
-            // Upper wall: bottom-right wall of up tile
-            // Local wall: top-left wall of up ghost tile
-            // Gap fill: 1px right edge from up_left tile
-            let upper = get_tile_id(Quadrant::BottomRight.offset(), neighbours.up, true);
-            let local = get_tile_id(Quadrant::TopLeft.offset(), them, true);
-            let gap = get_tile_id(Quadrant::TopRight.offset() + 2, neighbours.up_left, true);
-            (upper, local, Some(gap))
-        }
-        Quadrant::BottomLeft => {
-            // Upper wall: bottom-right wall of up_left tile
-            // Local wall: top-left wall of central tile
-            // Gap fill: 1px right edge from left tile
-            let upper = get_tile_id(Quadrant::BottomRight.offset(), neighbours.up_left, true);
-            let local = get_tile_id(Quadrant::TopLeft.offset(), me, true);
-            let gap = get_tile_id(Quadrant::TopRight.offset() + 2, neighbours.left, true);
-            (upper, local, Some(gap))
-        }
-        Quadrant::BottomRight => {
-            // Upper wall: bottom-left wall of up_right tile
-            // Local wall: top-right wall of central tile
-            let upper = get_tile_id(Quadrant::BottomLeft.offset(), neighbours.up_right, true);
-            let local = get_tile_id(Quadrant::TopRight.offset(), me, true);
-            (upper, local, None)
-        }
-    };
-
-    array::from_fn(|i| {
-        let i = i as u16;
-
-        fn get_tile(offset: u16) -> &'static [u32] {
-            tiles::ISOMETRIC.tiles.get_tile_data(offset)
-        }
-
-        let mut tile = DynamicTile16::new().fill_with(0);
-
-        let me_tile = get_tile(get_tile_id(quadrant.offset(), me, false) + i);
-        let them_tile = get_tile(get_tile_id(quadrant.reverse().offset(), them, false) + i);
-
-        // Order floors so higher-priority tile types render on top
-        let (first, second) = if me > them {
-            (me_tile, them_tile)
-        } else {
-            (them_tile, me_tile)
-        };
-
-        if let Some(gap_fill) = gap_fill
-            && i == 0
-        {
-            blit_16_colour(tile.data_mut(), get_tile(gap_fill));
-        }
-
-        blit_16_colour(tile.data_mut(), get_tile(upper_wall + i));
-        blit_16_colour(tile.data_mut(), get_tile(local_wall + i));
-
-        blit_16_colour(tile.data_mut(), first);
-        blit_16_colour(tile.data_mut(), second);
-
-        tile
-    })
-}
-
-impl Quadrant {
-    fn offset(self) -> u16 {
-        match self {
-            Quadrant::TopLeft => 0,
-            Quadrant::TopRight => 2,
-            Quadrant::BottomLeft => tiles::ISOMETRIC.width as u16,
-            Quadrant::BottomRight => tiles::ISOMETRIC.width as u16 + 2,
-        }
-    }
-
-    fn reverse(self) -> Self {
-        match self {
-            Quadrant::TopLeft => Quadrant::BottomRight,
-            Quadrant::TopRight => Quadrant::BottomLeft,
-            Quadrant::BottomLeft => Quadrant::TopRight,
-            Quadrant::BottomRight => Quadrant::TopLeft,
-        }
-    }
-
-    /// Returns the position in the direction of this quadrant
-    fn neighbour(self, pos: Vector2D<i32>) -> Vector2D<i32> {
-        let neighbour_quadrant = match self {
-            Quadrant::TopLeft => (-1, 0),
-            Quadrant::TopRight => (0, -1),
-            Quadrant::BottomLeft => (0, 1),
-            Quadrant::BottomRight => (1, 0),
-        };
-
-        pos + vec2(neighbour_quadrant.0, neighbour_quadrant.1)
-    }
-
-    fn from_gba_tile(tile: Vector2D<i32>) -> Self {
-        match (
-            (div_floor(tile.x, TILE_WIDTH / 2)).rem_euclid(TILE_WIDTH / 2),
-            tile.y.rem_euclid(TILE_HEIGHT),
-        ) {
-            (0, 0) => Quadrant::TopLeft,
-            (1, 0) => Quadrant::TopRight,
-            (0, 1) => Quadrant::BottomLeft,
-            (1, 1) => Quadrant::BottomRight,
-            _ => unreachable!(),
-        }
-    }
-}
-
-struct Map {
-    map_data: Vec<TileType>,
-    width: usize,
-    height: usize,
-}
-
-const TILE_WIDTH: i32 = 4;
-const TILE_HEIGHT: i32 = 2;
-
-impl Map {
-    fn new(width: usize, height: usize, map_data: Vec<TileType>) -> Self {
-        assert_eq!(map_data.len(), width * height);
-
-        Self {
-            map_data,
-            width,
-            height,
-        }
-    }
-
-    fn get_tile(&self, pos: Vector2D<i32>) -> TileType {
-        let Vector2D { x, y } = pos;
-        if x < 0 || x as usize >= self.width || y < 0 || y as usize >= self.height {
-            return TileType::Air;
-        }
-
-        self.map_data[x as usize + y as usize * self.width]
-    }
-
-    fn get_from_gba_tile(&self, x: i32, y: i32) -> TileSpec {
-        let quadrant = Quadrant::from_gba_tile(vec2(x, y));
-
-        let macro_tile_x = div_floor(x, TILE_WIDTH);
-        let macro_tile_y = div_floor(y, TILE_HEIGHT);
-
-        let tile = vec2(macro_tile_x + macro_tile_y, macro_tile_y - macro_tile_x);
-
-        let me = self.get_tile(tile);
-        let neighbour = self.get_tile(quadrant.neighbour(tile));
-
-        TileSpec {
-            quadrant,
-            me,
-            them: neighbour,
-            neighbours: NeighbourTileContext {
-                left: self.get_tile(tile + vec2(-1, 1)),
-                up_left: self.get_tile(tile + vec2(-1, 0)),
-                up: self.get_tile(tile + vec2(-1, -1)),
-                up_right: self.get_tile(tile + vec2(0, -1)),
-            },
-        }
-    }
-}
-
-/// same as the div_floor in nightly but isn't stable yet so here until that's stabilised
-fn div_floor(numerator: i32, divisor: i32) -> i32 {
-    let d = numerator / divisor;
-    let r = numerator % divisor;
-    let correction = (numerator ^ divisor) >> (i32::BITS - 1);
-    if r != 0 { d + correction } else { d }
-}
-
 struct Character {
     tag: &'static Tag,
     // position is the current foot location in world space
@@ -516,11 +248,7 @@ impl Character {
             Priority::P1
         };
 
-        let macro_space = vec2(
-            self.position.x - self.position.y + 1,
-            self.position.x + self.position.y + 1,
-        ) / 2;
-        let real_tile_space = vec2(macro_space.x * TILE_WIDTH, macro_space.y * TILE_HEIGHT);
+        let real_tile_space = world_to_gba_tile_smooth(self.position);
         let real_pixel_space = (real_tile_space * 8).round();
 
         Object::new(self.tag.sprite(0))
