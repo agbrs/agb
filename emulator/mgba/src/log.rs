ad326d4de5234e039f84f9464ce7657e975c19de
Gwilym Inzani <email@gwilym.dev>
2026-05-20 16:34:37 +0100

    Different types on Windows yay

--- emulator/mgba/src/log.rs
+++ emulator/mgba/src/log.rs
@@ -79,7 +79,7 @@ impl Logger {
 extern "C" fn log_string_wrapper(
     logger: *mut mgba_sys::mLogger,
     category: i32,
-    level: u32,
+    level: mgba_sys::mLogLevel,
     format: *const i8,
     args: VaArgs,
 ) {
@@ -141,7 +141,7 @@ fn convert_to_string(format: *const i8, var_args: VaArgs) -> Option<String> {
 extern "C" fn no_log(
     _l: *mut mgba_sys::mLogger,
     _category: i32,
-    _level: u32,
+    _level: mgba_sys::mLogLevel,
     _format: *const i8,
     _var_args: VaArgs,
 ) {
